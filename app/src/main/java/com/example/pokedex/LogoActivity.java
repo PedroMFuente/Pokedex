de78265f4b673d033cf1621620eb8d96841c2044
PedroM <pedrodelafuentefdez@gmail.com>
2020-10-29 14:03:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pokedex/LogoActivity.java
@@ -0,0 +1,33 @@
+package com.example.pokedex;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
